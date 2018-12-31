dce0d8e86aa67dcc831a4a9895407b833d640c4f
Jasonccj <981015619@qq.com>
2018-12-31 23:49:00 +0800

    #配置webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ const config = {
                 loader: 'vue-loader'                    //处理.vue文件
             },
             {
+                test: /\.jsx$/,
+                loader: 'babel-loader'                  //处理jsx文件
+            },
+            {
                 test: /\.css$/,
                 use: [
                     'style-loader',                     //将css的样式写入到html里面去
@@ -29,6 +33,12 @@ const config = {
                 use: [
                     'style-loader',                     //将css写入到html中去
                     'css-loader',                       //css-loader处理css
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true,            //stylus-loader和postcss-loader自己都会生成sourceMap,如果前面stylus-loader已生成了sourceMap
+                        }                               //那么postcss-loader可以直接引用前面的sourceMap
+                    }
                     'stylus-loader'                     //处理stylus的css预处理器的问题件,转换成css后,抛给上一层的css-loader
                 ]
             },

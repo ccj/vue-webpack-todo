3f0c26c57c7ff4e9bbb7c990b4c269c078cd37eb
Jasonccj <981015619@qq.com>
2019-01-01 16:47:03 +0800

    #设置背景图片及透明蒙层

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,9 @@
 <template>
+    <div id="app">
+        <div id="cover">
 
+        </div>
+    </div>
 </template>
 
 <script>
@@ -8,8 +12,21 @@ export default {
 }
 </script>
 
-<style>
-#test{
-    color: red;
-}
+<style lang="stylus" scoped>
+#app
+    position absolute
+    left 0
+    right 0
+    top 0
+    bottom 0
+    #cover
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom 0   
+        background-color #999 
+        opacity 0.8
+        z-index -1
+
 </style>

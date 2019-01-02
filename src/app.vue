7bcd08fc36ab629ca4d4f8c2a3232bb68a28fd91
Jasonccj <981015619@qq.com>
2019-01-02 23:39:58 +0800

    #Todo.vue实现

--- src/app.vue
+++ src/app.vue
@@ -2,6 +2,7 @@
     <div id="app">
         <div id="cover"></div>
         <Header></Header>
+        <Todo></Todo>
         <Footer></Footer>
     </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import Header from './todo/header.vue'
 import Footer from './todo/footer.jsx'
+import Todo from './todo/todo.vue'
 
 export default {
     components: {
         Header,
-        Footer
+        Footer,
+        Todo
     }
 }
 </script>
---
+++ src/todo/todo.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="real-app">
+        <input 
+            type="text" 
+            class="add-input"
+            autofocus="autofocus"
+            placeholder="接下去要做什么?"
+            @keyup.enter="addTodo"
+        >
+    </section>
+</template>
+
+<script>
+export default {
+    methods: {
+        addTodo(){
+
+        }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+.real-app
+    width 600px
+    margin 0 auto
+    box-shadow 0 0 5px #666
+
+.add-input
+    position relative
+    margin 0
+    width 100%
+    font-size 24px
+    font-family inherit
+    font-weight inherit 
+    line-height 1.4em
+    border none
+    outline none 
+    color inherit 
+    box-sizing border-box
+    font-smmothing antialiased
+    padding 16px 16px 16px 36px
+    box-shadow inset 0 -2px 1px rgba(0, 0, 0, 0.03)
+    
+</style>
+
+
